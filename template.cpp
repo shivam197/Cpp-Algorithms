aaa215c5092518c83868571404611c34cab71b37
Shivam Sehgal <shivamsehgal1980@gmail.com>
2020-03-13 13:53:56 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define mod 1000000007
+#define FT() int t; scanf("%d",&t); while(t--)
+#define pb push_back
+#define nl printf("\n")
+#define fi(i,start,end) for(int i=start; i < end ; ++i)
+#define fd(i,end,start) for(int i=end-1;i>=start; --i)
+#define ip(n)	scanf("%d",&n)
+#define op(n)	printf("%d",n)
+#define mz(a) 	memset(a,0,sizeof(a))
+#define inpArr(A,n) fi(i,0,n)	ip(A[i]);
+#define dispArr(A,n)	fi(i,0,n) printf("%d ",A[i]); nl;
+#define Fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+void solve()
+{
+	
+}
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	FT()
+		solve();
+	return 0;
+}
